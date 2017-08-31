5c519bc06716434258963800b37e76e6b9833fcf
Manuel Dalla Lana <manuel@pepeverde.agency>
2017-08-31 18:05:18 +0200

    Move db config to separate file

--- runtest.php
+++ runtest.php
@@ -9,33 +9,7 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 $config = new Configuration();
 
-$connectionParams['mysql55'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:55055/mailwatch',
-);
-$connectionParams['mysql56'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:55056/mailwatch',
-);
-$connectionParams['mysql57'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:55057/mailwatch',
-);
-$connectionParams['mysql80'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:55080/mailwatch',
-);
-$connectionParams['mariadb55'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:51055/mailwatch',
-);
-$connectionParams['mariadb100'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:51100/mailwatch',
-);
-$connectionParams['mariadb101'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:51101/mailwatch',
-);
-$connectionParams['mariadb102'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:51102/mailwatch',
-);
-$connectionParams['mariadb103'] = array(
-    'url' => 'mysql://mailwatch:mailwatch@127.0.0.1:51103/mailwatch',
-);
+$connectionParams = require __DIR__ . '/DBConfig.php';
 
 foreach ($connectionParams as $version => $connectionParam) {
     try {
